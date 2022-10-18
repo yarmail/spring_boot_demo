5583458662760f87f7b9ff0f1a3f363470d118bf
yarmail <yarmail@yandex.ru>
2022-10-18 23:15:09 +0300

    Initial commit

---
+++ src/main/java/project/controllers/HelloController.java
@@ -0,0 +1,24 @@
+package project.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @Value("${hello}")
+    private String hello;
+
+    @GetMapping ("/hello")
+    public String hello() {
+        System.out.println(this.hello);
+        return "hello";
+    }
+}
+/** Примечания
+ @Value("${hello}")
+ мы можем использовать нами заданный ключ в
+ application.properties для подстановки его сюда
+
+
+ */
